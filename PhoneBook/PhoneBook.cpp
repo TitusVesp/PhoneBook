ca4cb389229e002387b5014592ad45b6e8edcba5
Kiril Riabov <kiril2906@hmail.com>
2019-12-16 16:55:53 +0200

    Добавьте файлы проекта.

---
+++ PhoneBook/PhoneBook.cpp
@@ -0,0 +1,13 @@
+﻿// PhoneBook.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+extern void Kontacts();
+
+int main()
+{
+	Kontacts();
+}
